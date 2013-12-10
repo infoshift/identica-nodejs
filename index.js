09c8610bf7ecb126d5bddce6e4ca20684583b3d7
Jesse Panganiban <me@jpanganiban.com>
2013-12-10 15:16:40 +0800

    Add voxeo authentication

--- index.js
+++ index.js
@@ -7,6 +7,34 @@ var Identica = function(options) {
 
 /**
  * options:
+ *   app_id
+ */
+Identica.prototype.voxeoAuthenticate = function(options, cb) {
+  // Check if some parameters are missing
+  var x =  _.all(['subscriber_number'], function(i) {
+    return _.include(_.keys(options), i);
+  });
+
+  if (!x) {
+    return cb("Missing parameters", null);
+  }
+
+  return request
+    .post(this.options.url + '/voxeo_authenticate')
+    .type('form')
+    .send(options)
+    .end(function(err, resp) {
+      if (err) return cb("Failed to connect", null);
+      if (resp.statusCode === 200) return cb(null, {
+        statusCode: resp.statusCode,
+        body: resp.body
+      });
+      return cb("Failed to authenticate", null);
+    });
+};
+
+/**
+ * options:
  *   access_token
  *   *subscriber_number
  *   sender_address
