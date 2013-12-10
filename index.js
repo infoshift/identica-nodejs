84186ccc6a6225afaa1255379528142e9aea5d2a
Jesse Panganiban <me@jpanganiban.com>
2013-12-10 15:05:37 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var request = require('superagent');
+var _ = require('underscore');
+
+var Identica = function(options) {
+  this.options = options;
+};
+
+
+/**
+ * options:
+ *   access_token
+ *   *subscriber_number
+ *   sender_address
+ *   scope
+ */
+Identica.prototype.authenticate = function(options, cb) {
+  // Check if some parameters are missing
+  var x =  _.all(['subscriber_number'], function(i) {
+    return _.include(_.keys(options), i);
+  });
+
+  if (!x) {
+    return cb("Missing parameters", null);
+  }
+
+  return request
+    .post(this.options.url + '/authenticate')
+    .type('form')
+    .send(options)
+    .end(function(err, resp) {
+      if (err) return cb("Failed to connect", null);
+      if (resp.statusCode === 200) return cb(null, {
+        statusCode: resp.statusCode,
+        body: resp.body
+      });
+      return cb("Failed to authenticate", null);
+    });
+};
+
+module.exports = Identica;
